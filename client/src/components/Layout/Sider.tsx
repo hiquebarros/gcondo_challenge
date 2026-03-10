9bac27d100efb892f5fc5b5a8e54ea241327b1a7
Henrique Barros <henrique1_barros@hotmail.com>
2026-03-10 13:30:50 -0300

    feat: navbar style and logo

--- client/src/components/Layout/Sider.tsx
+++ client/src/components/Layout/Sider.tsx
@@ -3,6 +3,10 @@ import AntdSider from 'antd/es/layout/Sider';
 
 import type { RenderSiderProps } from './Root';
 
+import logoUrl from '../../assets/logo.webp';
+
+import './Sider.css';
+
 type SiderProps = RenderSiderProps & Pick<MenuProps, 'items' | 'defaultSelectedKeys'>;
 
 export function Sider({ collapsed, onCollapse, items, defaultSelectedKeys }: SiderProps) {
@@ -16,7 +20,11 @@ export function Sider({ collapsed, onCollapse, items, defaultSelectedKeys }: Sid
             onCollapse={onCollapse}
             style={{ background: token.colorBgContainer }}
         >
+            <div className="layout-sider-logo">
+                <img src={logoUrl} alt="Logo" />
+            </div>
             <AntdMenu
+                className="layout-sider-menu"
                 mode="inline"
                 theme="dark"
                 items={items}
